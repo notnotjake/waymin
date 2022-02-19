afd89d2381c38feaacb8634469385d5c3bd4efb5
notnotjake <jake@notnotjake.com>
2022-02-19 17:46:09 -0500

    Adds timer to homepage

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 let target = document.querySelector('#target')
+target.innerText = 'Hi'
 
-console.log(target)
+let count = document.querySelector('p')
+let time = 0
+
+var timer = setInterval( () => {
+	count.innerText = time
+	time++
+}, 1000)
